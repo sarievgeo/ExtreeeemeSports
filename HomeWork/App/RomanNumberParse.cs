9d73a67cfe4f99ac38c6fa78bd4fe57853362e13
sarievgeo <75241201+sarievgeo@users.noreply.github.com>
2022-09-06 08:39:08 +0300

    Homwork 2 (Calc) - Done!

---
+++ HomeWork/App/RomanNumberParse.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorEX.App
+{
+    public class RomanNumber
+    {
+        public static int Parse(String str)
+        {
+            var digits = new Dictionary<char, int>()
+            {
+                { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 },
+                { 'C', 100 }, { 'D', 500 }, { 'M', 1000 }
+            };
+
+            char digit = str[str.Length - 1];
+
+            int res = digits[digit];
+
+            for (int i = str.Length - 2; i >= 0; i--)
+            {
+                if (digits[digit] > digits[str[i]])
+                {
+                    res -= digits[str[i]];
+                }
+                else
+                {
+                    res += digits[str[i]];
+                }
+                digit = str[i];
+            }
+
+            return res;
+        }
+    }
+
+    // public static int Parse(String str)
+    // {
+    //     char[] digits = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+    //     int[] digitValues = { 1, 5, 10, 50, 100, 500, 1000 };
+    //     // Якщо наступна цифра числа більша за поточну, то
+    //     // вона віднімається від результату, інакше додається
+    //     // IX : -1 + 10;  XC : -10 + 100;  XX : +10+10; CX : +100+10
+    //     // XIV : +10-1+5; XIIII : 10+1+1+1+1
+    //
+    //     int pos = str.Length - 1;  // позиція останньої цифри числа
+    //     char digit = str[pos];     // символ цифри
+    //     int ind = Array.IndexOf(digits, digit);  // позиція цифри у масиві
+    //     if(ind == -1)
+    //     {
+    //         throw new ArgumentException($"Invalid char {digit}");
+    //     }
+    //     int val = digitValues[ind];  // величина цифри
+    //     int res = val;
+    //
+    //     pos -= 1;  // передостання цифра у числі
+    //     // Визначаємо величину цифри
+    //     // порівнюємо з наступною (останньою) цифрою (одержана вище)
+    //     // додаємо або віднімаємо в залежності від рез. порівняння
+    //     // продовжуємо доки pos не дійде до 0
+    //
+    //     return 1;
+    // }
+}
---
+++ TestProject/Calc_Test.cs
@@ -0,0 +1,32 @@
+using CalculatorEX.App;
+using HomeWork.App;
+
+namespace TestProject
+{
+    [TestClass]
+    public class Calc_Test
+    {
+        [TestMethod]
+        public void CalcTest()
+        {
+            var calc = new Calc();
+
+            Assert.IsNotNull(calc);
+        }
+
+        [TestMethod]
+        public void RomanNumberParse()
+        {
+            Assert.AreEqual(RomanNumber.Parse("I"), 1, "I == 1");
+            Assert.AreEqual(RomanNumber.Parse("IV"), 4, "IV == 4");
+            Assert.AreEqual(RomanNumber.Parse("XV"), 15);
+            Assert.AreEqual(RomanNumber.Parse("XXX"), 30);
+            Assert.AreEqual(RomanNumber.Parse("CM"), 900);
+            Assert.AreEqual(RomanNumber.Parse("MCMXCIX"), 1999);
+            Assert.AreEqual(RomanNumber.Parse("CD"), 400);
+            Assert.AreEqual(RomanNumber.Parse("CDI"), 401);
+            Assert.AreEqual(RomanNumber.Parse("LV"), 55);
+            Assert.AreEqual(RomanNumber.Parse("XL"), 40);
+        }
+    }
+}
