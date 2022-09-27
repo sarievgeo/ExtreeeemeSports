a1e061c60f6fc4c5f0e590d09eaf29f54ac081bd
sarievgeo <75241201+sarievgeo@users.noreply.github.com>
2022-09-27 08:58:18 +0300

    Ui add. End of Homework v2.0

--- HomeWork/App/Calc.cs
+++ HomeWork/App/Calc.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CalculatorEX.App;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +12,73 @@ namespace HomeWork.App
     {
         public void Run()
         {
-            Console.WriteLine("Hell");
-            Console.WriteLine("Error");
-            Console.WriteLine("Carrot");
+            AskLang();
+            RomanNumber res = null!;
+
+            // repeats untill user enter valid expression
+            do
+            {
+                // ask Expression
+                Console.Write(Localise.EnterExpression());
+                // user input expression
+                String? userInput = Console.ReadLine() ?? "";
+
+                try
+                {
+                    // evaluate expression
+                    res = EvalExpression(userInput);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            while (res is null);
+
+            // Show result
+            Console.WriteLine(Resources.Ui.Result(res));
+        }
+
+        public RomanNumber EvalExpression(String expression)
+        {
+            // split expression on two numbers and operation
+            string[] parts = expression.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            // expression is invalid
+            if (parts.Length != 3)
+                throw new ArgumentException(Resources.Ui.InvalidExpressionMessage(expression));
+
+            // operation is invalid
+            if (Array.IndexOf(RomanNumber.Operations, parts[1]) == -1)
+                throw new ArgumentException(Resources.Ui.InvalidOperationMessage(parts[1]));
+
+            var rn1 = new RomanNumber(RomanNumber.Parse(parts[0]));     // build roman number 1
+            var rn2 = new RomanNumber(RomanNumber.Parse(parts[2]));     // build roman number 2
+            var res = parts[1] == RomanNumber.Operations[0]             // counts result of expression
+                ? rn1.Add(rn2)
+                : rn1.Sub(rn2);
+
+            return res;
+        }
+
+        private void AskLang()
+        {
+            while (true)
+            {
+                Console.Write(Resources.Ui.AskLang());
+                var lang = Console.ReadLine();
+
+                if (lang == "1")
+                    Resources.Culture = "en-US";
+                else if (lang == "2")
+                    Resources.Culture = "uk-UA";
+
+                Console.Clear();
+
+                if (lang is "1" or "2")
+                    return;
+            }
+
         }
     }
 }
--- HomeWork/App/Localise.cs
+++ HomeWork/App/Localise.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CalculatorEX.App;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HomeWork.App
@@ -72,6 +74,68 @@ namespace HomeWork.App
                 case "en-US": return "'N' is not allowed in this context";
             }
             throw new Exception("Unsupported language");
+        }
+
+        public static class Ui
+        {
+            public static string AskLang()
+                => "Choose language / Оберіть мову\n    1: English\n    2: Українська\n    -> ";
+
+            public static string Number(int number, string? language = null)
+                => (language ?? Language) switch
+                {
+                    "uk-UA" => $"Римське число #{number}: ",
+                    "en-US" => $"Roman number #{number}: ",
+                    _ => throw new ArgumentException()
+                };
+
+            public static string Result(RomanNumber res, string? language = null)
+                => (language ?? Language) switch
+                {
+                    "uk-UA" => $"Результат -> {res} / {res.Value}",
+                    "en-US" => $"Result -> {res} / {res.Value}",
+                    _ => throw new ArgumentException()
+                };
+
+            public static string EnterNumber(string? language = null)
+                => (language ?? Language) switch
+                {
+                    "uk-UA" => "Введіть число: ",
+                    "en-US" => "Enter number: ",
+                    _ => throw new ArgumentException()
+                };
+
+            public static string EnterOperation(string? language = null)
+                => (language ?? Language) switch
+                {
+                    "uk-UA" => "Введіть операцію: ",
+                    "en-US" => "Enter operation: ",
+                    _ => throw new ArgumentException()
+                };
+
+            public static string EnterExpression(string? language = null)
+                => (language ?? Language) switch
+                {
+                    "uk-UA" => $"Введіть вираз: ",
+                    "en-US" => $"Input Expression: ",
+                    _ => throw new ArgumentException()
+                };
+
+            public static string InvalidExpressionMessage(string expression, string? language = null)
+                => (language ?? Language) switch
+                {
+                    "uk-UA" => $"Неприпустимий вираз: {expression}",
+                    "en-US" => $"Invalid Expression: {expression}",
+                    _ => throw new ArgumentException()
+                };
+
+            public static string InvalidOperationMessage(string operation, string? language = null)
+                => (language ?? Language) switch
+                {
+                    "uk-UA" => $"Неприпустима операція {operation}",
+                    "en-US" => $"Invalid Operation: {operation}",
+                    _ => throw new ArgumentException()
+                };
         }
     }
 }
